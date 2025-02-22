cdd6fe4773f62b81994940e5ddc0b27a95f40418
jeff930 <jefferson30.jdg@gmail.com>
2025-02-22 23:19:47 +0800

    Added authentication APIs

--- auth-service/routes/api.php
+++ auth-service/routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/check', function () {
     return response()->json(['message' => 'Auth API is working!']);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });

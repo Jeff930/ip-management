d19e1c09bb819fe0160a537d3d4ed082508ce17c
jeff930 <jefferson30.jdg@gmail.com>
2025-03-10 17:16:14 +0800

    Updated gateway configuration to auth-service

--- gateway/src/server.js
+++ gateway/src/server.js
@@ -1,19 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
+app.use(bodyParser.json());
 
-app.use("/auth", createProxyMiddleware({
-    target: "http://ip-auth-service:9000",
-    changeOrigin: true
-}));
+app.get("/", (req, res) => {
+    console.log("test")
+    res.send("Gateway is running!");
+});
 
-app.use("/ip", createProxyMiddleware({
-    target: "http://ip-management-service:3000",
-    changeOrigin: true
-}));
+app.use(
+    "/auth",
+    createProxyMiddleware({
+        target: "http://ip-auth-nginx",
+        changeOrigin: true,
+    })
+);
+
+app.use(
+    "/ip",
+    createProxyMiddleware({
+        target: "http://ip-management-service:3000",
+        changeOrigin: true,
+    })
+);
 
 const PORT = 8080;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Gateway running on port ${PORT}`);
 });

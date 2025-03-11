30f9c76a93da3e9bfdddb1f028bc494e3f38cacc
jeff930 <jefferson30.jdg@gmail.com>
2025-03-11 15:48:12 +0800

    Removed user log creation in ip-service

--- ip-service/src/routes/auditRoutes.js
+++ ip-service/src/routes/auditRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const AuditLog = require("../models/AuditLog");
 const authenticateToken = require("../middlewares/authenticateToken");
-const { logAction } = require("../services/auditLogService");
 
 const router = express.Router();
 
@@ -28,50 +27,5 @@ router.get("/", authenticateToken, checkPermission("view-logs"), async (req, res
     res.status(500).json({ error: "Failed to fetch logs", details: error.message });
   }
 });
-
-router.post("/log-login", authenticateToken, async (req, res) => {
-  const LoginUserId = req.user.sub;
-  const LoginUserName = req.user.user_name;
-  try {
-
-    await logAction({
-      actorId: LoginUserId,
-      sessionId: req.user.session_id,
-      actorName: LoginUserName,
-      action: "LOGIN",
-      targetId: LoginUserId,
-      targetType: "USER",
-      target: "SELF",
-    });
-
-    res.json({message: "success" });
-  } catch (error) {
-    console.error("❌ Failed to log login event:", error);
-    res.status(500).json({ error: "Failed to log login event.", details: error.message });
-  }
-});
-
-router.post("/log-logout", authenticateToken, async (req, res) => {
-  const LogoutUserId = req.user.sub;
-  const LogoutUserName = req.user.user_name;
-  try {
-
-    await logAction({
-      actorId: LogoutUserId,
-      sessionId: req.user.session_id,
-      actorName: LogoutUserName,
-      action: "LOGOUT",
-      targetId: LogoutUserId,
-      targetType: "USER",
-      target: "SELF",
-    });
-
-    res.json({ message: "success" });
-  } catch (error) {
-    console.error("❌ Failed to log logout event:", error);
-    res.status(500).json({ error: "Failed to log logout event:", details: error.message });
-  }
-});
-
 
 module.exports = router;
